0272f9025dfdc7a1abcafc45a1d0da7b1c70a5b4
ateshima <andrewteshima@gmail.com>
2016-04-05 15:48:27 -0400

    Finish Mikes Blog

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :text_messages
   get '/text' => "home#index"
   post '/send_text' => "home#send_text"
+  Rails.application.routes.draw do
+  post 'test' => 'test#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
